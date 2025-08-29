5a297a0568e7a818681da00a4362032609f5787d
Nazar Chykur <nazar.chykur@firstgroup.com>
2025-08-29 16:01:38 +0300

    git init enum demo project

---
+++ src/main/java/org/example/enumdemo/EnumdemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.enumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnumdemoApplication.class, args);
+    }
+
+}
